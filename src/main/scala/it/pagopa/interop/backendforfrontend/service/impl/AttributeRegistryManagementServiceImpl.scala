89132390f5a69aee9548039fff0df9132d020474
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-06-01 12:12:41 +0200

    fix log

--- src/main/scala/it/pagopa/interop/backendforfrontend/service/impl/AttributeRegistryManagementServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/service/impl/AttributeRegistryManagementServiceImpl.scala
@@ -1,8 +1,8 @@
 package it.pagopa.interop.backendforfrontend.service.impl
 
-import com.typesafe.scalalogging.Logger
+import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
 import it.pagopa.interop.attributeregistrymanagement.client.api.AttributeApi
-import it.pagopa.interop.attributeregistrymanagement.client.invoker.BearerToken
+import it.pagopa.interop.attributeregistrymanagement.client.invoker.{ApiError, BearerToken}
 import it.pagopa.interop.attributeregistrymanagement.client.model.AttributesResponse
 import it.pagopa.interop.backendforfrontend.service.AttributeRegistryManagementService
 import it.pagopa.interop.backendforfrontend.service.types.AttributeRegistryServiceTypes.{
@@ -11,15 +11,23 @@ import it.pagopa.interop.backendforfrontend.service.types.AttributeRegistryServi
 }
 import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 import it.pagopa.interop.commons.utils.TypeConversions.EitherOps
+import it.pagopa.interop.commons.utils.errors.GenericComponentErrors.{
+  GenericClientError,
+  ResourceNotFoundError,
+  ThirdPartyCallError
+}
 import it.pagopa.interop.commons.utils.extractHeaders
 
 import scala.concurrent.{ExecutionContext, Future}
 
-final case class AttributeRegistryManagementServiceImpl(invoker: AttributeRegistryManagementInvoker, api: AttributeApi)(
-  implicit ec: ExecutionContext
+final case class AttributesApiServiceImpl(invoker: AttributeRegistryManagementInvoker, api: AttributeApi)(implicit
+  ec: ExecutionContext
 ) extends AttributeRegistryManagementService {
 
   implicit val logger = Logger.takingImplicit[ContextFieldsToLog](this.getClass)
+
+  private val replacementEntityId: String = "NoIdentifier"
+  private val serviceName: String         = "attribute-registry"
 
   override def getAttributes(
     search: Option[String]
@@ -27,7 +35,7 @@ final case class AttributeRegistryManagementServiceImpl(invoker: AttributeRegist
     for {
       (bearerToken, correlationId, ip) <- extractHeaders(contexts).toFuture
       request = api.getAttributes(xCorrelationId = correlationId, xForwardedFor = ip)(BearerToken(bearerToken))
-      result <- invoker.invoke(request, s"Loading attributes")
+      result <- invoker.invoke(request, s"Loading attributes", invocationRecovery(search))
     } yield result
   }
 
@@ -39,6 +47,25 @@ final case class AttributeRegistryManagementServiceImpl(invoker: AttributeRegist
       request = api.getAttributeByOriginAndCode(xCorrelationId = correlationId, origin, code, xForwardedFor = ip)(
         BearerToken(bearerToken)
       )
-      result <- invoker.invoke(request, s"Getting attribute by origin = $origin and code = $code")
+      result <- invoker.invoke(
+        request,
+        s"Getting attribute by origin = $origin and code = $code",
+        invocationRecovery(Some(s"$origin/$code"))
+      )
     } yield result
+
+  private def invocationRecovery[T](
+    entityId: Option[String]
+  ): (ContextFieldsToLog, LoggerTakingImplicit[ContextFieldsToLog], String) => PartialFunction[Throwable, Future[T]] =
+    (context, logger, msg) => {
+      case ex @ ApiError(code, message, _, _, _) if code == 404 =>
+        logger.error(s"$msg. code > $code - message > $message", ex)(context)
+        Future.failed[T](ResourceNotFoundError(entityId.getOrElse(replacementEntityId)))
+      case ex @ ApiError(code, message, _, _, _)                =>
+        logger.error(s"$msg. code > $code - message > $message", ex)(context)
+        Future.failed[T](ThirdPartyCallError(serviceName, ex.getMessage))
+      case ex                                                   =>
+        logger.error(s"$msg. Error: ${ex.getMessage}", ex)(context)
+        Future.failed[T](GenericClientError(ex.getMessage))
+    }
 }
