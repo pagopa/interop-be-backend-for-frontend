2a44ee0ae29679f884222a07bf11f0c5fcd5924f
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2022-06-01 11:58:51 +0200

    PIN-1355 (#10)
    * PIN-1355:
     - add getInstitution function from party
     - add query param to route /institutions/{institutionId}/relationships to apply filter on name / family name
    
    * PIN-1355: fix formatting

--- src/main/scala/it/pagopa/interop/backendforfrontend/service/types/PartyProcessServiceTypes.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/service/types/PartyProcessServiceTypes.scala
@@ -10,4 +10,6 @@ object PartyProcessServiceTypes {
   type PartyProcessPartyRole         = partyprocess.client.model.PartyRole
   type PartyProcessRelationshipState = partyprocess.client.model.RelationshipState
   type PartyProcessProductInfo       = partyprocess.client.model.ProductInfo
+  type PartyProcessAttribute         = partyprocess.client.model.Attribute
+  type PartyProcessInstitution       = partyprocess.client.model.Institution
 }
