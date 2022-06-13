9e5a9bb5d1c15a0bb2334b8f346d821bf06902a4
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-06-13 10:32:52 +0200

    update PagopaVersions

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,10 +15,10 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion           = "0.1.x-SNAPSHOT"
-  lazy val partyProcessVersion      = "0.1.x-SNAPSHOT"
-  lazy val partyManagementVersion   = "0.1.x-SNAPSHOT"
-  lazy val userRegistryVersion      = "0.1.x-SNAPSHOT"
-  lazy val attributeRegistryVersion = "0.1.x-SNAPSHOT"
+  lazy val commonsVersion           = "0.1.8"
+  lazy val partyProcessVersion      = "0.1.0"
+  lazy val partyManagementVersion   = "0.1.0"
+  lazy val userRegistryVersion      = "0.1.0"
+  lazy val attributeRegistryVersion = "0.1.7"
 
 }
