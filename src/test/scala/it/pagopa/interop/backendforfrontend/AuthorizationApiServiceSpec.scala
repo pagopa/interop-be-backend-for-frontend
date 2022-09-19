8fe0a40f0db972ce9a0010aa751372e0ae6a7027
Antonio Gelameris <toniogela89@gmail.com>
2022-09-19 17:02:03 +0200

    "Fixed tests" (#26)
    * Fixed tests
    
    * extended test case

--- src/test/scala/it/pagopa/interop/backendforfrontend/AuthorizationApiServiceSpec.scala
+++ src/test/scala/it/pagopa/interop/backendforfrontend/AuthorizationApiServiceSpec.scala
@@ -2,7 +2,6 @@ package it.pagopa.interop.backendforfrontend
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.nimbusds.jwt.JWTClaimsSet
 import it.pagopa.interop.backendforfrontend.api.impl.AuthorizationApiMarshallerImpl._
 import it.pagopa.interop.backendforfrontend.common.system.ApplicationConfiguration
 import it.pagopa.interop.backendforfrontend.model.{IdentityToken, Problem, SessionToken}
@@ -12,21 +11,25 @@ import org.scalatest.wordspec.AnyWordSpecLike
 
 import java.util.UUID
 import scala.concurrent.Future
-import scala.jdk.CollectionConverters.{MapHasAsJava, SeqHasAsJava}
 import scala.util.{Failure, Success}
+import SpecHelper._
 
 class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with ScalatestRouteTest {
 
-  "Generating a session token" ignore {
+  "Generating a session token" should {
 
     "succeed" in {
 
-      val uid: String                       = UUID.randomUUID().toString
-      val organization: Map[String, String] = Map("id" -> "id", "role" -> "role", "fiscalCode" -> "fiscalCode")
-      val claimSet: Map[String, AnyRef]     = Map("uid" -> uid, "organization" -> organization.asJava)
+      val uid: String = UUID.randomUUID().toString
 
-      val builder: JWTClaimsSet.Builder = new JWTClaimsSet.Builder()
-      val jwtClaimsSet: JWTClaimsSet    = builder.claim("uid", uid).claim("organization", organization.asJava).build()
+      val jwtClaimsSet = Map(
+        "organization" -> Map(
+          "id"         -> "id",
+          "fiscalCode" -> "fiscalCode",
+          "roles"      -> List(Map("role" -> "admin"), Map("role" -> "anotherRole"))
+        ),
+        "uid"          -> uid
+      ).asClaimSet
 
       (mockJwtReader
         .getClaims(_: String))
@@ -34,11 +37,22 @@ class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with S
         .once()
         .returns(Success(jwtClaimsSet))
 
+      val desiredClaimSet: Map[String, AnyRef] = Map(
+        "uid"            -> uid,
+        "user-roles"     -> "admin,anotherRole",
+        "organizationId" -> "id",
+        "organization"   -> Map(
+          "id"         -> "id",
+          "fiscalCode" -> "fiscalCode",
+          "roles"      -> List(Map("role" -> "admin"), Map("role" -> "anotherRole"))
+        ).toJSONObject
+      )
+
       (mockSessionTokenGenerator
         .generate(_: SignatureAlgorithm, _: Map[String, AnyRef], _: Set[String], _: String, _: Long))
         .expects(
           SignatureAlgorithm.RSAPkcs1Sha256,
-          claimSet,
+          desiredClaimSet,
           ApplicationConfiguration.generatedJwtAudience,
           ApplicationConfiguration.generatedJwtIssuer,
           ApplicationConfiguration.generatedJwtDuration
@@ -73,15 +87,12 @@ class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with S
 
     "fail on SessionTokenGenerator failure" in {
 
-      val uid: String                       = UUID.randomUUID().toString
-      val organization: Map[String, AnyRef] =
-        Map("id" -> "id", "roles" -> List(Map("role" -> "admin").asJava).asJava, "fiscalCode" -> "fiscalCode")
+      val uid: String = UUID.randomUUID().toString
 
-      val claimSet: Map[String, AnyRef] =
-        Map("uid" -> uid, "organization" -> organization.asJava, "user-roles" -> "admin", "organizationId" -> "id")
-
-      val builder: JWTClaimsSet.Builder = new JWTClaimsSet.Builder()
-      val jwtClaimsSet: JWTClaimsSet    = builder.claim("uid", uid).claim("organization", organization.asJava).build()
+      val jwtClaimsSet = Map(
+        "organization" -> Map("id" -> "id", "fiscalCode" -> "fiscalCode", "roles" -> List(Map("role" -> "admin"))),
+        "uid"          -> uid
+      ).asClaimSet
 
       (mockJwtReader
         .getClaims(_: String))
@@ -89,11 +100,22 @@ class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with S
         .once()
         .returns(Success(jwtClaimsSet))
 
+      val desiredClaimSet: Map[String, AnyRef] = Map(
+        "uid"            -> uid,
+        "user-roles"     -> "admin",
+        "organizationId" -> "id",
+        "organization"   -> Map(
+          "id"         -> "id",
+          "fiscalCode" -> "fiscalCode",
+          "roles"      -> List(Map("role" -> "admin"))
+        ).toJSONObject
+      )
+
       (mockSessionTokenGenerator
         .generate(_: SignatureAlgorithm, _: Map[String, AnyRef], _: Set[String], _: String, _: Long))
         .expects(
           SignatureAlgorithm.RSAPkcs1Sha256,
-          claimSet,
+          desiredClaimSet,
           ApplicationConfiguration.generatedJwtAudience,
           ApplicationConfiguration.generatedJwtIssuer,
           ApplicationConfiguration.generatedJwtDuration
