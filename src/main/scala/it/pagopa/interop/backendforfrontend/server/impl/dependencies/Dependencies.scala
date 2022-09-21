6ec1aafb10e79d98522458d6c92345c38457dcb6
galales <25105748+galales@users.noreply.github.com>
2022-09-21 18:55:52 +0200

    pin-1858: Remove unused import

--- src/main/scala/it/pagopa/interop/backendforfrontend/server/impl/dependencies/Dependencies.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/server/impl/dependencies/Dependencies.scala
@@ -55,7 +55,7 @@ import it.pagopa.interop.commons.jwt.service.impl.{DefaultJWTReader, DefaultSess
 import it.pagopa.interop.commons.signer.service.SignerService
 import it.pagopa.interop.commons.signer.service.impl.KMSSignerService
 import it.pagopa.interop.commons.utils.TypeConversions.TryOps
-import it.pagopa.interop.commons.utils.errors.{ComponentError, GenericComponentErrors}
+import it.pagopa.interop.commons.utils.errors.GenericComponentErrors
 import it.pagopa.interop.commons.utils.service.impl.OffsetDateTimeSupplierImpl
 import it.pagopa.interop.commons.utils.{AkkaUtils, OpenapiUtils}
 import it.pagopa.interop.selfcare.partyprocess.client.api.ProcessApi
