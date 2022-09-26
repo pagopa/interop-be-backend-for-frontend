6221347184bb4680ae7297edc56d7a0438454b0f
galales <25105748+galales@users.noreply.github.com>
2022-09-26 11:02:04 +0200

    pin-1858-headers: Rate limit headers

--- src/main/scala/it/pagopa/interop/backendforfrontend/error/Handlers.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/error/Handlers.scala
@@ -12,6 +12,7 @@ import it.pagopa.interop.backendforfrontend.model.Problem
 import it.pagopa.interop.catalogmanagement.client.invoker.{ApiError => CatalogManagementError}
 import it.pagopa.interop.commons.logging.ContextFieldsToLog
 import it.pagopa.interop.commons.ratelimiter
+import it.pagopa.interop.commons.ratelimiter.model.{Headers, RateLimitStatus}
 import it.pagopa.interop.commons.utils.errors.GenericComponentErrors.{GenericError, TooManyRequests}
 import it.pagopa.interop.selfcare.partyprocess.client.invoker.{ApiError => PartyProcessError}
 import it.pagopa.interop.selfcare.userregistry.client.invoker.{ApiError => UserRegistryError}
@@ -27,14 +28,14 @@ object Handlers {
     logger: LoggerTakingImplicit[ContextFieldsToLog],
     toEntityMarshallerProblem: ToEntityMarshaller[Problem]
   ): PartialFunction[Try[_], StandardRoute] = log(logMessage) andThen {
-    case Failure(err: AgreementProcessError[_])               => completeWithError(err.message)
-    case Failure(err: AttributeRegistryError[_])              => completeWithError(err.message)
-    case Failure(err: CatalogManagementError[_])              => completeWithError(err.message)
-    case Failure(err: TenantManagementError[_])               => completeWithError(err.message)
-    case Failure(err: PartyProcessError[_])                   => completeWithError(err.message)
-    case Failure(err: UserRegistryError[_])                   => completeWithError(err.message)
-    case Failure(_ @ratelimiter.error.Errors.TooManyRequests) => tooManyRequests()
-    case Failure(_)                                           => internalServerError(logMessage)
+    case Failure(err: AgreementProcessError[_])                 => completeWithError(err.message)
+    case Failure(err: AttributeRegistryError[_])                => completeWithError(err.message)
+    case Failure(err: CatalogManagementError[_])                => completeWithError(err.message)
+    case Failure(err: TenantManagementError[_])                 => completeWithError(err.message)
+    case Failure(err: PartyProcessError[_])                     => completeWithError(err.message)
+    case Failure(err: UserRegistryError[_])                     => completeWithError(err.message)
+    case Failure(tmr: ratelimiter.error.Errors.TooManyRequests) => tooManyRequests(tmr.status)
+    case Failure(_)                                             => internalServerError(logMessage)
   }
 
   private def log(logMessage: String)(implicit
@@ -54,8 +55,14 @@ object Handlers {
     complete(problem.status, problem)
   }
 
-  private def tooManyRequests()(implicit toEntityMarshallerProblem: ToEntityMarshaller[Problem]): StandardRoute =
-    complete(StatusCodes.TooManyRequests, problemOf(StatusCodes.TooManyRequests, TooManyRequests))
+  private def tooManyRequests(
+    rateLimitStatus: RateLimitStatus
+  )(implicit toEntityMarshallerProblem: ToEntityMarshaller[Problem]): StandardRoute =
+    complete(
+      StatusCodes.TooManyRequests,
+      Headers.headersFromStatus(rateLimitStatus),
+      problemOf(StatusCodes.TooManyRequests, TooManyRequests)
+    )
 
   private def responseToProblem(problemBody: String): Problem =
     // Note: the body is actually a Problem of the below service, but the schema is the same,
