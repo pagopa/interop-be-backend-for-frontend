924b329c7ee47c2d833e05a125fe513eb0673b2d
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2022-10-06 15:12:23 +0200

    add internal token for session token generation (#42)
    * add internal token for session token generation
    
    * apply PR comments

--- src/test/scala/it/pagopa/interop/backendforfrontend/AuthorizationApiServiceSpec.scala
+++ src/test/scala/it/pagopa/interop/backendforfrontend/AuthorizationApiServiceSpec.scala
@@ -2,29 +2,27 @@ package it.pagopa.interop.backendforfrontend
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import cats.syntax.all._
+import com.typesafe.scalalogging.LoggerTakingImplicit
+import it.pagopa.interop.backendforfrontend.SpecHelper._
 import it.pagopa.interop.backendforfrontend.api.impl.AuthorizationApiMarshallerImpl._
 import it.pagopa.interop.backendforfrontend.common.system.ApplicationConfiguration
 import it.pagopa.interop.backendforfrontend.model.{IdentityToken, Problem, SessionToken}
+import it.pagopa.interop.commons.logging.ContextFieldsToLog
+import it.pagopa.interop.commons.ratelimiter.model.RateLimitStatus
 import it.pagopa.interop.commons.signer.model.SignatureAlgorithm
+import it.pagopa.interop.commons.utils.service.OffsetDateTimeSupplier
+import it.pagopa.interop.selfcare.partyprocess.client.model.Institution
+import it.pagopa.interop.tenantmanagement.client.invoker.ApiError
+import it.pagopa.interop.tenantmanagement.client.model.{ExternalId, Tenant}
+import it.pagopa.interop.tenantprocess
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpecLike
 
 import java.util.UUID
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
-import SpecHelper._
-import com.typesafe.scalalogging.LoggerTakingImplicit
-import it.pagopa.interop.commons.logging.ContextFieldsToLog
-import it.pagopa.interop.commons.ratelimiter.model.RateLimitStatus
-
-import cats.syntax.all._
-import it.pagopa.interop.tenantprocess
-import scala.concurrent.duration.DurationInt
-import it.pagopa.interop.tenantmanagement.client.model.Tenant
-import it.pagopa.interop.commons.utils.service.OffsetDateTimeSupplier
-import it.pagopa.interop.tenantmanagement.client.model.ExternalId
-import it.pagopa.interop.tenantmanagement.client.invoker.ApiError
-import it.pagopa.interop.selfcare.partyprocess.client.model.Institution
 
 class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with ScalatestRouteTest {
 
@@ -50,6 +48,12 @@ class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with S
         .expects(*)
         .once()
         .returns(Success(jwtClaimsSet))
+
+      (mockInteropTokenGenerator
+        .generateInternalToken(_: String, _: List[String], _: String, _: Long))
+        .expects(*, *, *, *)
+        .once()
+        .returns(Future.successful(internalToken))
 
       (mockTenantManagement
         .getBySelfcareId(_: String)(_: Seq[(String, String)]))
@@ -132,6 +136,12 @@ class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with S
         .expects(*)
         .once()
         .returns(Success(jwtClaimsSet))
+
+      (mockInteropTokenGenerator
+        .generateInternalToken(_: String, _: List[String], _: String, _: Long))
+        .expects(*, *, *, *)
+        .once()
+        .returns(Future.successful(internalToken))
 
       (mockTenantManagement
         .getBySelfcareId(_: String)(_: Seq[(String, String)]))
@@ -258,6 +268,12 @@ class AuthorizationApiServiceSpec extends AnyWordSpecLike with SpecHelper with S
         .expects(*)
         .once()
         .returns(Success(jwtClaimsSet))
+
+      (mockInteropTokenGenerator
+        .generateInternalToken(_: String, _: List[String], _: String, _: Long))
+        .expects(*, *, *, *)
+        .once()
+        .returns(Future.successful(internalToken))
 
       (mockTenantManagement
         .getBySelfcareId(_: String)(_: Seq[(String, String)]))
