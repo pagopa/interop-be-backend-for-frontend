1c1304356f31e54f94b495c267b2abc2ea69cf19
Fabrizio Musella <fabrizio.musella@pagopa.it>
2023-02-16 17:59:43 +0100

    deleted NoDescriptorInEservice handle (This time for real)

--- src/main/scala/it/pagopa/interop/backendforfrontend/error/Handlers.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/error/Handlers.scala
@@ -48,7 +48,6 @@ object Handlers {
     case Failure(err: UnknownTenantOrigin)                      => badRequest(err, logMessage)
     case Failure(err: InvalidInterfaceFileDetected)             => badRequest(err, logMessage)
     case Failure(err: AgreementDescriptorNotFound)              => notFound(err, logMessage)
-    case Failure(err: NoDescriptorInEservice)                   => notFound(err, logMessage)
     case Failure(err)                                           => internalServerError(err, logMessage)
   }
 
