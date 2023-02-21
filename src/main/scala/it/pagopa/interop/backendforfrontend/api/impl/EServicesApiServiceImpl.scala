41d9614794144c4e69f5fc8f04abcda6638a2527
Riccardo Torsoli <122275960+nttdata-rtorsoli@users.noreply.github.com>
2023-02-21 11:27:37 +0100

    PIN-2771 modify sort in getLatestAgreement (#110)
    Co-authored-by: Riccardo Torsoli <riccardo.torsoli@nttdata.com>
    Co-authored-by: Alessio Gallitano <25105748+galales@users.noreply.github.com>

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/EServicesApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/EServicesApiServiceImpl.scala
@@ -36,6 +36,7 @@ import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 import scala.xml.Elem
+import java.time.OffsetDateTime
 
 final case class EServicesApiServiceImpl(
   agreementProcessService: AgreementProcessService,
@@ -325,7 +326,11 @@ final case class EServicesApiServiceImpl(
 
   private def getLatestAgreement(requesterId: UUID, eService: CatalogProcess.EService)(implicit
     contexts: Seq[(String, String)]
-  ): Future[Option[AgreementProcess.Agreement]] =
+  ): Future[Option[AgreementProcess.Agreement]] = {
+
+    val ordering: Ordering[(Int, OffsetDateTime)] =
+      Ordering.Tuple2(Ordering.Int.reverse, Ordering.by[OffsetDateTime, Long](_.toEpochSecond).reverse)
+
     getAllAgreements(
       consumersIds = requesterId :: Nil,
       eServicesIds = eService.id :: Nil,
@@ -334,10 +339,11 @@ final case class EServicesApiServiceImpl(
     ).map(
       _.map(agreement => (agreement, eService.descriptors.find(_.id == agreement.descriptorId)))
         .collect { case (agreement, Some(descriptor)) => (agreement, descriptor) }
-        .sortBy(_._2.version.toInt)(Ordering[Int].reverse)
+        .sortBy(s => (s._2.version.toInt, s._1.createdAt))(ordering)
         .headOption
         .map(_._1)
     )
+  }
 
   private def enhanceCatalogEService(
     requesterId: UUID
