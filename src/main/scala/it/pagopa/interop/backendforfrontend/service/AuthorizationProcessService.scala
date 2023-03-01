08fb31a06f339603cf612c51f4c20cb30d53e904
Riccardo Torsoli <122275960+nttdata-rtorsoli@users.noreply.github.com>
2023-03-01 15:35:16 +0100

    PIN-1328 authorization-process DELETE /clients/{clientId}/purposes/{purposeId} (#129)
    * PIN-1328 Delete client purpose endpoint

--- src/main/scala/it/pagopa/interop/backendforfrontend/service/AuthorizationProcessService.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/service/AuthorizationProcessService.scala
@@ -1,8 +1,12 @@
 package it.pagopa.interop.backendforfrontend.service
 
+import java.util.UUID
 import scala.concurrent.Future
 
 trait AuthorizationProcessService {
 
   def deleteClient(clientId: String)(implicit contexts: Seq[(String, String)]): Future[Unit]
+
+  def removeClientPurpose(clientId: UUID, purposeId: UUID)(implicit contexts: Seq[(String, String)]): Future[Unit]
+
 }
