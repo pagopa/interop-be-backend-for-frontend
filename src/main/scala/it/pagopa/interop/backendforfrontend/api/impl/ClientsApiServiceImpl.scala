598fe5fcedf89e842e3113c38d04489bc6e6462b
Riccardo Torsoli <122275960+nttdata-rtorsoli@users.noreply.github.com>
2023-03-03 09:12:18 +0100

    PIN-1342 authorization-process DELETE /clients/{clientId}/relationships/{relationshipId} (#132)
    * PIN-1342 Remove Client Operator Relationship

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/ClientsApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/ClientsApiServiceImpl.scala
@@ -48,4 +48,23 @@ final case class ClientsApiServiceImpl(authorizationProcessService: Authorizatio
       }
     }
   }
+
+  override def removeClientOperatorRelationship(clientId: String, relationshipId: String)(implicit
+    contexts: Seq[(String, String)],
+    toEntityMarshallerProblem: ToEntityMarshaller[Problem]
+  ): Route = {
+
+    val result: Future[Unit] = for {
+      clientUuid       <- clientId.toFutureUUID
+      relationshipUuid <- relationshipId.toFutureUUID
+      _                <- authorizationProcessService.removeClientOperatorRelationship(clientUuid, relationshipUuid)
+    } yield ()
+
+    onComplete(result) {
+      handleError(s"Error removing operator relationship $relationshipId of client $clientId") orElse {
+        case Success(_) =>
+          removeClientOperatorRelationship204
+      }
+    }
+  }
 }
