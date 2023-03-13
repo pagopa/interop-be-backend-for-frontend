c90e94b6dc45433b6253cbeef2497c3f8e05c397
Riccardo Torsoli <riccardo.torsoli@nttdata.com>
2023-03-13 15:54:30 +0100

    PIN-1325 Create endpoint and linked to authorization process

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/ClientsApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/ClientsApiServiceImpl.scala
@@ -9,7 +9,7 @@ import akka.http.scaladsl.server.Directives.onComplete
 import akka.http.scaladsl.server.Route
 import it.pagopa.interop.backendforfrontend.error.Handlers.handleError
 import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
-import it.pagopa.interop.backendforfrontend.model.{Problem, PurposeAdditionDetailsSeed, CreatedResource}
+import it.pagopa.interop.backendforfrontend.model._
 import it.pagopa.interop.backendforfrontend.service.types.AuthorizationProcessServiceTypes._
 
 import scala.concurrent.{Future, ExecutionContext}
@@ -120,6 +120,22 @@ final case class ClientsApiServiceImpl(authorizationProcessService: Authorizatio
       handleError(s"Error adding purpose ${purposeAdditionDetailsSeed.purposeId} to client $clientId") orElse {
         case Success(_) =>
           addClientPurpose204
+      }
+    }
+  }
+
+  override def createApiClient(clientSeed: ClientSeed)(implicit
+    contexts: Seq[(String, String)],
+    toEntityMarshallerProblem: ToEntityMarshaller[Problem],
+    toEntityMarshallerCreatedResource: ToEntityMarshaller[CreatedResource]
+  ): Route = {
+
+    val result: Future[CreatedResource] =
+      authorizationProcessService.createApiClient(clientSeed.toProcess) map (_.toCreatedResource)
+
+    onComplete(result) {
+      handleError(s"Error creating api client with name ${clientSeed.name}") orElse { case Success(resource) =>
+        createApiClient201(resource)
       }
     }
   }
--- src/main/scala/it/pagopa/interop/backendforfrontend/service/types/AuthorizationProcessServiceTypes.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/service/types/AuthorizationProcessServiceTypes.scala
@@ -2,6 +2,7 @@ package it.pagopa.interop.backendforfrontend.service.types
 
 import it.pagopa.interop.authorizationprocess.client.{model => AuthorizationProcess}
 import it.pagopa.interop.backendforfrontend.model.{PurposeAdditionDetailsSeed, CreatedResource}
+import it.pagopa.interop.backendforfrontend.model.ClientSeed
 
 object AuthorizationProcessServiceTypes {
 
@@ -12,6 +13,11 @@ object AuthorizationProcessServiceTypes {
 
   implicit class ClientConverter(private val client: AuthorizationProcess.Client) extends AnyVal {
     def toCreatedResource: CreatedResource = CreatedResource(id = client.id)
+  }
+
+  implicit class ClientSeedConverter(private val seed: ClientSeed) extends AnyVal {
+    def toProcess: AuthorizationProcess.ClientSeed =
+      AuthorizationProcess.ClientSeed(name = seed.name, description = seed.description)
   }
 
 }
