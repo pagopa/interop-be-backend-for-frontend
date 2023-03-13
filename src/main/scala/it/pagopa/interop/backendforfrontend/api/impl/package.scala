d98f7f309956aeec7b445b91ffa7c591f57f8930
Riccardo Torsoli <riccardo.torsoli@nttdata.com>
2023-03-13 11:50:45 +0100

    PIN-2886 Refactoring names

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/package.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/package.scala
@@ -164,7 +164,7 @@ package object impl extends SprayJsonSupport with DefaultJsonProtocol {
     : RootJsonFormat[WaitingForApprovalPurposeVersionUpdateContentSeed] =
     jsonFormat1(WaitingForApprovalPurposeVersionUpdateContentSeed)
 
-  implicit val compactProductFormat: RootJsonFormat[SelfcareProduct] = jsonFormat2(SelfcareProduct)
+  implicit val selfcareProductFormat: RootJsonFormat[SelfcareProduct] = jsonFormat2(SelfcareProduct)
 
   final val entityMarshallerProblem: ToEntityMarshaller[Problem] = sprayJsonMarshaller[Problem]
 
