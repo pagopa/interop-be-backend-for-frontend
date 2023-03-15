e42c54841367d4348057c6c24774a3ff8fc1f8af
Riccardo Torsoli <riccardo.torsoli@nttdata.com>
2023-03-15 17:16:36 +0100

    PIN-1339 Merge 1.0.x

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/ClientsApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/ClientsApiMarshallerImpl.scala
@@ -20,7 +20,7 @@ object ClientsApiMarshallerImpl extends ClientsApiMarshaller with SprayJsonSuppo
   override implicit def toEntityMarshallerOperatorarray: ToEntityMarshaller[Seq[Operator]] =
     sprayJsonMarshaller[Seq[Operator]]
 
-  override implicit def fromEntityUnmarshallerKeySeedList: FromEntityUnmarshaller[Seq[KeySeed]] =
+  override implicit def fromEntityUnmarshallerKeySeedList: FromEntityUnmarshaller[Seq[KeySeed]]  =
     sprayJsonUnmarshaller[Seq[KeySeed]]
   override implicit def toEntityMarshallerEncodedClientKey: ToEntityMarshaller[EncodedClientKey] =
     sprayJsonMarshaller[EncodedClientKey]
