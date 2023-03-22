a3f7684cb08e960b8f0dc90028f570670c9099da
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-03-22 11:48:16 +0100

    Revert "Pin 1329 authorization-process GET /clients/{clientId}/keys (#139)" (#156)
    This reverts commit 6a64d01e8568414d32febdc578a411dd98c4a835.

--- src/main/scala/it/pagopa/interop/backendforfrontend/server/impl/dependencies/Dependencies.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/server/impl/dependencies/Dependencies.scala
@@ -246,7 +246,7 @@ trait Dependencies {
 
     val clientsApi: ClientsApi =
       new ClientsApi(
-        ClientsApiServiceImpl(authorizationProcess, tenantProcess, catalogProcess, purposeProcess, partyProcess),
+        ClientsApiServiceImpl(authorizationProcess, tenantProcess, catalogProcess, purposeProcess),
         ClientsApiMarshallerImpl,
         oauthAndRateLimitingDirective
       )
