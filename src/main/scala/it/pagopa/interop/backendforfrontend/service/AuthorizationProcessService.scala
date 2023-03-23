4824215b748c2edc21b40882c3deb207fffb68f6
Riccardo Torsoli <122275960+nttdata-rtorsoli@users.noreply.github.com>
2023-03-23 15:59:39 +0100

    PIN-3136 [BE] Implementazione endpoint GET /clients/{clientId}/relationships/{relationshipId} (#159)

--- src/main/scala/it/pagopa/interop/backendforfrontend/service/AuthorizationProcessService.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/service/AuthorizationProcessService.scala
@@ -52,4 +52,8 @@ trait AuthorizationProcessService {
     offset: Int
   )(implicit contexts: Seq[(String, String)]): Future[Clients]
 
+  def getClientOperatorRelationshipById(clientId: UUID, relationshipId: UUID)(implicit
+    contexts: Seq[(String, String)]
+  ): Future[Operator]
+
 }
