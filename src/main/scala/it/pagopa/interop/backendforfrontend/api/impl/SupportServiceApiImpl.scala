1082472531b123e11d1961a34489a847f3080617
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-06-06 11:08:33 +0200

    close example

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/SupportServiceApiImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/SupportServiceApiImpl.scala
@@ -1,8 +1,7 @@
 package it.pagopa.interop.backendforfrontend.api.impl
 
-import akka.http.scaladsl.model.headers.{HttpCookie, Location, `Set-Cookie`}
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
-import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives.{complete, redirect}
 import akka.http.scaladsl.server.Route
 import it.pagopa.interop.backendforfrontend.api.SupportApiService
 import it.pagopa.interop.backendforfrontend.model.SAMLResponse
@@ -18,25 +17,8 @@ class SupportServiceApiImpl extends SupportApiService {
     saml.response.encodeBase64 match {
       case Success(base64) =>
         val redirectUrl =
-          s"https://selfcare.dev.interop.pagopa.it/ui/it/assistenza/scelta-ente"
-        val samlCookie  = HttpCookie("saml2", base64)
-          .withSecure(true)
-          .withHttpOnly(true)
-        val jwt         = HttpCookie("token", "jwt")
-          .withSecure(true)
-          .withHttpOnly(true)
-        val headers     = List(Location(redirectUrl), `Set-Cookie`(samlCookie), `Set-Cookie`(jwt))
-
-        complete(
-          HttpResponse(
-            status = StatusCodes.MovedPermanently,
-            headers = headers,
-            entity = StatusCodes.MovedPermanently.htmlTemplate match {
-              case ""       => HttpEntity.Empty
-              case template => HttpEntity(ContentTypes.`text/html(UTF-8)`, template format redirectUrl)
-            }
-          )
-        )
+          s"https://selfcare.dev.interop.pagopa.it/ui/it/assistenza/scelta-ente#saml2=$base64&jwt=<add_jwt>"
+        redirect(redirectUrl, StatusCodes.MovedPermanently)
       case Failure(ex)     =>
         ex.printStackTrace()
         complete(StatusCodes.BadRequest)
