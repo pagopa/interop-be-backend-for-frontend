482dcba60803122674bd2da7e234a3d7ef4e460c
AsterITA <fabrizio.musella@gmail.com>
2023-06-23 15:21:42 +0200

    PIN-3510: removed VerificationRenewal (#189)

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/TenantsApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/TenantsApiMarshallerImpl.scala
@@ -35,7 +35,7 @@ object TenantsApiMarshallerImpl extends TenantsApiMarshaller with SprayJsonSuppo
   override implicit def toEntityMarshallerTenant: ToEntityMarshaller[Tenant] =
     sprayJsonMarshaller[Tenant]
 
-  override implicit def fromEntityUnmarshallerRenewalVerifiedTenantAttributeSeed
-    : FromEntityUnmarshaller[RenewalVerifiedTenantAttributeSeed] =
-    sprayJsonUnmarshaller[RenewalVerifiedTenantAttributeSeed]
+  override implicit def fromEntityUnmarshallerUpdateVerifiedTenantAttributeSeed
+    : FromEntityUnmarshaller[UpdateVerifiedTenantAttributeSeed] =
+    sprayJsonUnmarshaller[UpdateVerifiedTenantAttributeSeed]
 }
