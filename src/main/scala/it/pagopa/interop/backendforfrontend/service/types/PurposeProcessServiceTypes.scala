d834fabdbd52367d2b8429084b1545a2d17bac84
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-10-04 18:10:43 +0200

    PIN-3987-2: Update reverse purpose

--- src/main/scala/it/pagopa/interop/backendforfrontend/service/types/PurposeProcessServiceTypes.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/service/types/PurposeProcessServiceTypes.scala
@@ -159,6 +159,17 @@ object PurposeProcessServiceTypes {
       )
   }
 
+  implicit class ReversePurposeUpdateContentConverter(private val puc: ReversePurposeUpdateContent) extends AnyVal {
+    def toProcess: PurposeProcess.PurposeUpdateContent =
+      PurposeProcess.PurposeUpdateContent(
+        title = puc.title,
+        description = puc.description,
+        isFreeOfCharge = puc.isFreeOfCharge,
+        freeOfChargeReason = puc.freeOfChargeReason,
+        dailyCalls = puc.dailyCalls
+      )
+  }
+
   implicit class RiskAnalysisFormConfigWrapper(
     private val riskAnalysisFormConfig: PurposeProcess.RiskAnalysisFormConfigResponse
   ) extends AnyVal {
