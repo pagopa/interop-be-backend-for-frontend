ce2403c25749c3d33b588a5a5fce91fbd6ce3f0f
AsterITA <fabrizio.musella@gmail.com>
2023-10-24 09:58:13 +0200

    PIN-4067: added contactMail on GET Agreement's Producer - DO NOT MERGE (#230)

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/AgreementsApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/AgreementsApiServiceImpl.scala
@@ -339,7 +339,8 @@ final case class AgreementsApiServiceImpl(
     producer = CompactOrganization(
       id = agreement.producerId,
       name = producerTenant.name,
-      kind = producerTenant.kind.map(_.toApi)
+      kind = producerTenant.kind.map(_.toApi),
+      contactMail = producerTenant.mails.find(_.kind == TenantProcess.MailKind.CONTACT_EMAIL).map(_.toApi)
     ),
     consumer = Tenant(
       id = agreement.consumerId,
