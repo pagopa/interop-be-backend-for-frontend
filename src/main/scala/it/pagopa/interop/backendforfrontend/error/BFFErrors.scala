733e34943097ac64c46a57012302cd83dfb05230
AsterITA <fabrizio.musella@gmail.com>
2024-05-30 14:45:51 +0200

    typo

--- src/main/scala/it/pagopa/interop/backendforfrontend/error/BFFErrors.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/error/BFFErrors.scala
@@ -150,7 +150,7 @@ object BFFErrors {
   final case class ContractException(agreementId: String)
       extends ComponentError("0036", s"Contract exception for agreement $agreementId")
 
-  final case class MissinInterface(eServiceId: UUID, descriptorId: UUID)
+  final case class MissingInterface(eServiceId: UUID, descriptorId: UUID)
       extends ComponentError("0037", s"Missing interface for Eservice $eServiceId and descriptor $descriptorId")
 
   final case class NotValidDescriptor(descriptorId: String, descriptorStatus: String)
--- src/main/scala/it/pagopa/interop/backendforfrontend/error/Handlers.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/error/Handlers.scala
@@ -51,7 +51,7 @@ object Handlers {
     case Failure(err: InvalidInterfaceContentTypeDetected) => badRequest(err, logMessage, headers)
     case Failure(err: InvalidInterfaceFileDetected)        => badRequest(err, logMessage, headers)
     case Failure(err: CreateDocumentBadRequest)            => badRequest(err, logMessage, headers)
-    case Failure(err: MissinInterface)                     => badRequest(err, logMessage, headers)
+    case Failure(err: MissingInterface)                    => badRequest(err, logMessage, headers)
     case Failure(ex: NotValidDescriptor)                   => badRequest(ex, logMessage)
     case Failure(err: InvalidEServiceRequester)            => forbidden(err, logMessage, headers)
     case Failure(ex: OperationForbidden.type)              => forbidden(ex, logMessage)
