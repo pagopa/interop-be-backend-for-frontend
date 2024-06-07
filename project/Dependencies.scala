e61a62860d55c58c6d943ef6e1531aef3fb9aef3
AsterITA <fabrizio.musella@gmail.com>
2024-06-07 17:27:13 +0200

    removed circe dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -118,13 +118,6 @@ object Dependencies {
     lazy val testkit = "org.scanamo" %% "scanamo-testkit" % scanamoVersion
   }
 
-  private[this] object circe {
-    lazy val namespace = "io.circe"
-    lazy val core      = namespace %% "circe-core"    % circeVersion
-    lazy val generic   = namespace %% "circe-generic" % circeVersion
-    lazy val parser    = namespace %% "circe-parser"  % circeVersion
-  }
-
   object Jars {
     lazy val overrides: Seq[ModuleID] =
       Seq(jackson.annotations % Compile, jackson.core % Compile, jackson.databind % Compile)
@@ -164,9 +157,6 @@ object Dependencies {
       tika.core                                % Compile,
       scanamo.scanamo                          % Compile,
       opensaml.core                            % Compile,
-      circe.core                               % Compile,
-      circe.generic                            % Compile,
-      circe.parser                             % Compile,
       akka.httpTestkit                         % Test,
       akka.testkit                             % Test,
       akka.untypedTestkit                      % Test,
