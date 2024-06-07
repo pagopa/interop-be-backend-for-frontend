4ba4c9aa4cb5a06b415b6dabd0db6432ac0ae769
AsterITA <fabrizio.musella@gmail.com>
2024-06-07 17:27:33 +0200

    added ImportedEservice class

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/package.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/package.scala
@@ -1,7 +1,7 @@
 package it.pagopa.interop.backendforfrontend.api
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.marshalling.{ToEntityMarshaller, Marshaller}
+import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
 import akka.http.scaladsl.model.StatusCode
 import it.pagopa.interop.backendforfrontend.model._
 import it.pagopa.interop.backendforfrontend.service.types.CatalogProcessServiceTypes.EServiceConsumer
@@ -14,10 +14,18 @@ import it.pagopa.interop.commons.utils.errors.{ComponentError, ServiceCode}
 import it.pagopa.interop.commons.utils.{BEARER, UID}
 import spray.json._
 import akka.http.scaladsl.model.ContentTypes
+import it.pagopa.interop.backendforfrontend.service.model.{
+  ImportedDescriptor,
+  ImportedDoc,
+  ImportedEservice,
+  ImportedMultiAnswer,
+  ImportedRiskAnalysis,
+  ImportedSingleAnswer
+}
+
 import java.nio.charset.StandardCharsets
 import scala.io.{BufferedSource, Codec}
 import java.io.File
-
 import scala.concurrent.{ExecutionContext, Future}
 
 package object impl extends SprayJsonSupport with DefaultJsonProtocol {
@@ -47,6 +55,13 @@ package object impl extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val catalogEServicesFormat: RootJsonFormat[CatalogEServices]         = jsonFormat2(CatalogEServices)
   implicit val producerEServiceFormat: RootJsonFormat[ProducerEService]         = jsonFormat5(ProducerEService)
   implicit val producerEServicesFormat: RootJsonFormat[ProducerEServices]       = jsonFormat2(ProducerEServices)
+
+  implicit val importedDocFormat: RootJsonFormat[ImportedDoc]                   = jsonFormat2(ImportedDoc)
+  implicit val importedDescriptorFormat: RootJsonFormat[ImportedDescriptor]     = jsonFormat8(ImportedDescriptor)
+  implicit val importedSingleAnswerFormat: RootJsonFormat[ImportedSingleAnswer] = jsonFormat2(ImportedSingleAnswer)
+  implicit val importedMultiAnswerFormat: RootJsonFormat[ImportedMultiAnswer]   = jsonFormat2(ImportedMultiAnswer)
+  implicit val importedRiskAnalysisFormat: RootJsonFormat[ImportedRiskAnalysis] = jsonFormat3(ImportedRiskAnalysis)
+  implicit val importedEserviceFormat: RootJsonFormat[ImportedEservice]         = jsonFormat6(ImportedEservice)
 
   implicit val declaredTenantAttributeSeedFormat: RootJsonFormat[DeclaredTenantAttributeSeed] =
     jsonFormat1(DeclaredTenantAttributeSeed)
--- src/main/scala/it/pagopa/interop/backendforfrontend/service/model/ImportedEservice.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/service/model/ImportedEservice.scala
@@ -1,34 +1,32 @@
 package it.pagopa.interop.backendforfrontend.service.model
 
-import io.circe.{Decoder, Encoder}
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
-import it.pagopa.interop.backendforfrontend.model.{AgreementApprovalPolicy, EServiceMode, EServiceTechnology}
+import it.pagopa.interop.catalogprocess.client.model.{AgreementApprovalPolicy, EServiceMode, EServiceTechnology}
 
-final case class ImportedDoc(prettyName: String, name: String)
+
+final case class ImportedDoc(prettyName: String, path: String)
 final case class ImportedDescriptor(
-                       interface: Option[ImportedDoc],
-                       docs: Seq[ImportedDoc],
-                       audience: Seq[String],
-                       voucherLifespan: Int,
-                       dailyCallsPerConsumer: Int,
-                       dailyCallsTotal: Int,
-                       description: Option[String],
-                       agreementApprovalPolicy: AgreementApprovalPolicy
-                     )
+  interface: Option[ImportedDoc],
+  docs: Seq[ImportedDoc],
+  audience: Seq[String],
+  voucherLifespan: Int,
+  dailyCallsPerConsumer: Int,
+  dailyCallsTotal: Int,
+  description: Option[String],
+  agreementApprovalPolicy: AgreementApprovalPolicy
+)
 final case class ImportedSingleAnswer(key: String, value: Option[String])
 final case class ImportedMultiAnswer(key: String, values: Seq[String])
-final case class ImportedRiskAnalysis(version: String, singleAnswers: Seq[ImportedSingleAnswer], multiAnswers: Seq[ImportedMultiAnswer])
+final case class ImportedRiskAnalysis(
+  version: String,
+  singleAnswers: Seq[ImportedSingleAnswer],
+  multiAnswers: Seq[ImportedMultiAnswer]
+)
 
-final case class ImportedEservice (
-                                    name: String,
-                                    description: String,
-                                    technology: EServiceTechnology,
-                                    mode: EServiceMode,
-                                    descriptor: ImportedDescriptor,
-                                    riskAnalysis: Option[ImportedRiskAnalysis]
-                                  )
-
-object ImportedEservice {
-  implicit val decoder: Decoder[ImportedEservice] = deriveDecoder
-  implicit val encoder: Encoder[ImportedEservice] = deriveEncoder
-}
+final case class ImportedEservice(
+  name: String,
+  description: String,
+  technology: EServiceTechnology,
+  mode: EServiceMode,
+  descriptor: ImportedDescriptor,
+  riskAnalysis: Option[ImportedRiskAnalysis]
+)
