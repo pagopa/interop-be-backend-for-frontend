d04a22babba7293c69d6f5de960e12bc68597445
AsterITA <fabrizio.musella@gmail.com>
2024-06-10 16:22:25 +0200

    fix tests

--- src/test/scala/it/pagopa/interop/backendforfrontend/FileManagerUtilsSpec.scala
+++ src/test/scala/it/pagopa/interop/backendforfrontend/FileManagerUtilsSpec.scala
@@ -1,3 +1,5 @@
+import akka.http.scaladsl.model.{ContentTypes, HttpCharsets, MediaType}
+import akka.http.scaladsl.server.directives.FileInfo
 import it.pagopa.interop.backendforfrontend.common.system.FileManagerUtils
 import it.pagopa.interop.backendforfrontend.error.BFFErrors.InvalidInterfaceContentTypeDetected
 import it.pagopa.interop.catalogprocess.client.model._
@@ -5,6 +7,7 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import java.io.File
 import java.nio.file.{Files, Paths}
 import java.util.UUID
 
@@ -25,52 +28,87 @@ class FileManagerUtilsSpec() extends AnyWordSpec with Matchers with ScalaFutures
   "a FileManagerUtils.verify " should {
     "succeed with a JSON file" in {
 
-      val file = Files.readAllBytes(Paths.get("src/test/resources/apis.json"))
+      val file = Paths.get("src/test/resources/apis.json").toFile
+
+      val fileParts: (FileInfo, File) =
+        (FileInfo(fieldName = "apis", fileName = "apis.json", contentType = ContentTypes.`application/json`), file)
 
       FileManagerUtils
-        .verify(file = file, filename = "apis.json", eService = eServiceRest, isInterface = true)(
-          Seq.empty
-        ) shouldBe Right(())
+        .verify(fileParts = fileParts, eService = eServiceRest, isInterface = true)(Seq.empty) shouldBe Right(())
     }
 
     "succeed with a YAML file" in {
 
-      val file = Files.readAllBytes(Paths.get("src/test/resources/apis.json"))
+      val file = Paths.get("src/test/resources/apis.json").toFile
+
+      val fileParts: (FileInfo, File) =
+        (
+          FileInfo(
+            fieldName = "apis",
+            fileName = "apis.yaml",
+            contentType =
+              MediaType.customWithFixedCharset("application", "x-yaml", HttpCharsets.`UTF-8`, List("yaml", "yml"))
+          ),
+          file
+        )
 
       FileManagerUtils
-        .verify(file = file, filename = "apis.json", eService = eServiceRest, isInterface = true)(
-          Seq.empty
-        ) shouldBe Right(())
+        .verify(fileParts = fileParts, eService = eServiceRest, isInterface = true)(Seq.empty) shouldBe Right(())
     }
 
     "succeed with a WSDL file" in {
 
-      val file = Files.readAllBytes(Paths.get("src/test/resources/apis.wsdl"))
+      val file = Paths.get("src/test/resources/apis.wsdl").toFile
+
+      val fileParts: (FileInfo, File) =
+        (
+          FileInfo(
+            fieldName = "apis",
+            fileName = "apis.wsdl",
+            contentType =
+              MediaType.customWithFixedCharset("application", "wsdl+xml", HttpCharsets.`UTF-8`, List("wsdl"))
+          ),
+          file
+        )
 
       FileManagerUtils
-        .verify(file = file, filename = "apis.wsdl", eService = eServiceSoap, isInterface = true)(
-          Seq.empty
-        ) shouldBe Right(())
+        .verify(fileParts = fileParts, eService = eServiceSoap, isInterface = true)(Seq.empty) shouldBe Right(())
     }
 
     "succeed with a XML file" in {
 
-      val file = Files.readAllBytes(Paths.get("src/test/resources/apis.xml"))
+      val file = Paths.get("src/test/resources/apis.xml").toFile
+
+      val fileParts: (FileInfo, File) =
+        (
+          FileInfo(
+            fieldName = "apis",
+            fileName = "apis.xml",
+            contentType = MediaType.customWithFixedCharset("application", "soap+xml", HttpCharsets.`UTF-8`, List("xml"))
+          ),
+          file
+        )
 
       FileManagerUtils
-        .verify(file = file, filename = "apis.xml", eService = eServiceSoap, isInterface = true)(
-          Seq.empty
-        ) shouldBe Right(())
+        .verify(fileParts = fileParts, eService = eServiceSoap, isInterface = true)(Seq.empty) shouldBe Right(())
     }
 
     "fail for unexpected file format" in {
 
-      val file = Files.readAllBytes(Paths.get("src/test/resources/apis.conf"))
+      val file = Paths.get("src/test/resources/apis.conf").toFile
+
+      val fileParts: (FileInfo, File) =
+        (
+          FileInfo(
+            fieldName = "apis",
+            fileName = "apis.conf",
+            contentType = MediaType.customWithFixedCharset("text", "plain", HttpCharsets.`UTF-8`, List("conf"))
+          ),
+          file
+        )
 
       FileManagerUtils
-        .verify(file = file, filename = "apis.conf", eService = eServiceSoap, isInterface = true)(
-          Seq.empty
-        ) shouldBe Left(
+        .verify(fileParts = fileParts, eService = eServiceSoap, isInterface = true)(Seq.empty) shouldBe Left(
         InvalidInterfaceContentTypeDetected(eServiceSoap.id.toString, "text/x-config", eServiceSoap.technology.toString)
       )
     }
