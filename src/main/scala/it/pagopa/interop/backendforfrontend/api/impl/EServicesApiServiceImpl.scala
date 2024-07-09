89a160bb793f1c20779f47403cd20c84badf5441
AsterITA <fabrizio.musella@gmail.com>
2024-07-09 16:53:59 +0200

    PIN-5050: Support docs with same name in exportEservice (#299)

--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/EServicesApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/EServicesApiServiceImpl.scala
@@ -17,6 +17,7 @@ import it.pagopa.interop.backendforfrontend.api.EservicesApiService
 import it.pagopa.interop.backendforfrontend.api.impl.Utils.{
   assertRequesterAllowed,
   canBeUpgraded,
+  generateUniqueFileName,
   pollEServiceById,
   verifyAndCreateEServiceDocument
 }
@@ -48,6 +49,7 @@ import java.time.format.DateTimeFormatter
 import java.util.UUID
 import java.util.concurrent.TimeUnit
 import java.util.zip.{ZipEntry, ZipInputStream, ZipOutputStream}
+import scala.collection.mutable
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters.IteratorHasAsScala
@@ -872,6 +874,8 @@ final case class EServicesApiServiceImpl(
   ): Route = {
 
     def extractConfig(eService: CatalogProcess.EService, descriptor: CatalogProcess.EServiceDescriptor): Json = {
+      val nameCounts = mutable.Map[String, Int]()
+
       JsonObject(
         "name"         -> Json.fromString(eService.name),
         "description"  -> Json.fromString(eService.description),
@@ -884,13 +888,11 @@ final case class EServicesApiServiceImpl(
             )
             .getOrElse(Json.Null),
           "docs"                    -> Json.arr(
-            descriptor.docs.map(doc =>
+            descriptor.docs.map(doc => {
+              val newName = generateUniqueFileName(nameCounts, doc.name)
               Json
-                .obj(
-                  "prettyName" -> Json.fromString(doc.prettyName),
-                  "path"       -> Json.fromString(s"documents/${doc.name}")
-                )
-            ): _* // `: _*` is used to convert Seq to varargs for Json.arr
+                .obj("prettyName" -> Json.fromString(doc.prettyName), "path" -> Json.fromString(s"documents/$newName"))
+            }): _* // `: _*` is used to convert Seq to varargs for Json.arr
           ),
           "audience"                -> Json.arr(descriptor.audience.map(Json.fromString): _*),
           "voucherLifespan"         -> Json.fromInt(descriptor.voucherLifespan),
@@ -938,11 +940,13 @@ final case class EServicesApiServiceImpl(
       interfaceFile: (Array[Byte], String),
       config: Json
     ): Array[Byte] = {
-      val bos    = new ByteArrayOutputStream()
-      val zipOut = new ZipOutputStream(bos)
+      val bos        = new ByteArrayOutputStream()
+      val zipOut     = new ZipOutputStream(bos)
+      val nameCounts = mutable.Map[String, Int]()
 
       docs.foreach { case (data, entryName) =>
-        zipOut.putNextEntry(new ZipEntry(s"$folderName/documents/$entryName"))
+        val newName = generateUniqueFileName(nameCounts, entryName)
+        zipOut.putNextEntry(new ZipEntry(s"$folderName/documents/$newName"))
         zipOut.write(data)
         zipOut.closeEntry()
       }
--- src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/Utils.scala
+++ src/main/scala/it/pagopa/interop/backendforfrontend/api/impl/Utils.scala
@@ -42,6 +42,7 @@ import java.time.{Instant, OffsetDateTime, ZoneOffset}
 import java.util
 import java.util.UUID
 import javax.xml.parsers.DocumentBuilderFactory
+import scala.collection.mutable
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters._
@@ -333,5 +334,17 @@ object Utils {
     }
 
     poll(1)
+  }
+
+  def generateUniqueFileName(nameCounts: mutable.Map[String, Int], originalName: String): String = {
+    val extensionIndex = originalName.lastIndexOf('.')
+    val baseName       = if (extensionIndex != -1) originalName.substring(0, extensionIndex) else originalName
+    val extension      = if (extensionIndex != -1) originalName.substring(extensionIndex) else ""
+
+    val count   = nameCounts.getOrElse(originalName, 0)
+    val newName = if (count > 0) s"$baseName-$count$extension" else originalName
+
+    nameCounts(originalName) = count + 1
+    newName
   }
 }
